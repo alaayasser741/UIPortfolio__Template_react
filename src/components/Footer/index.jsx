7c65db270b182f92d40d18bedda6d5e5e55c8e8e
Alaa Yasser <alaayasser2018@gmail.com>
2023-02-21 20:29:26 +0200

    add footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer style={{fontSize:'1rem',color:'#777',textAlign:"center",padding:"2rem 0"}}>
+            Made With Love ❤️ by (Alaa Yasser)
+        </footer>
     );
 }
 export default Footer;

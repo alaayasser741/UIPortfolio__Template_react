42a81ce87f118d0a8944c53e2174a64943cb7297
Alaa Yasser <alaayasser2018@gmail.com>
2023-02-21 20:15:55 +0200

    add content component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import './style.css'
 const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className="Contact__Section">
+            <div className="container">
+                <h2>Drop Me A Line</h2>
+                <form>
+                    <div className="input__info">
+                    <input required type="text" placeholder="Your Name" />
+                    <input required type="email" placeholder="Your Email"/>
+                    </div>
+                    <textarea required placeholder="Your Message"></textarea>
+                    <input type="submit" value="Send Message" />
+                </form>
+            </div>
         </div>
     );
 }

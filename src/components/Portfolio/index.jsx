f2c08113b91cd6709f19ad2c8d14ddeb6906aff6
Alaa Yasser <alaayasser2018@gmail.com>
2023-02-21 17:49:35 +0200

    add Portfolio Components

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Img__gallery, Portfolio__img, Img__box, Portfolio__title, Portfolio__section } from './style'
+import axios from "axios";
 const Portfolio = () => {
+    const [images, setImages] = useState([]);
+    useEffect(() => {
+        try {
+            const getImg = async () => {
+                const Response = await axios.get('js/data.json');
+                setImages(Response.data.portfolio);
+            }
+            getImg();
+        } catch (e) {
+            console.log(e)
+        }
+    }, []);
+    const PortfolioSection = images.map(data => {
+        return (
+            <Img__box key={data.id}>
+                <Portfolio__img src={data.image} alt={"portfolio-img " + data.id} />
+            </Img__box>
+        )
+    })
     return (
-        <div>
-            Portfolio
-        </div>
+        <Portfolio__section>
+            <Portfolio__title>My Portfolio</Portfolio__title>
+            <Img__gallery>
+                {PortfolioSection}
+            </Img__gallery>
+        </Portfolio__section>
     );
 }
 export default Portfolio;
---
+++ src/components/Portfolio/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+export const Portfolio__section = styled.div`
+  margin-block-end: 6rem;
+`;
+export const Portfolio__title = styled.h2`
+  text-align: center;
+  margin-block-end: 3rem;
+  font-size:2.5rem;
+`;
+export const Img__gallery = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+`;
+export const Img__box = styled.div``;
+export const Portfolio__img = styled.img`
+  width: 100%;
+`;
